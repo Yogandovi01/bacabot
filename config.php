8dc19384f36d48199844fcd63f05dd72e7875f7b
Yogandovi01 <31580579+Yogandovi01@users.noreply.github.com>
2019-04-13 12:50:01 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="Facebook_2097328853678800";
+$raw_uid="a3d84cf6-889d-46ac-9604-73bbbe314a99";
+$user_name="Yoga+Badru+Jaman";
+$invite_code="F9bYeKxJCq";
+$user_token="Aby5oUuY4Ifg6EQw";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="5feb89838bfabd7a";
+$device_type="asus++ASUS_A007";
+
+
+?>
